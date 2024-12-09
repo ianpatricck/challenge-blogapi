eead3420049596aa0c6c11e46ae4471cc1d84fe0
ianpatricck <ianpatrick499@gmail.com>
2024-12-09 10:16:05 -0300

    refactor(CommentController): throwing ModelNotFoundException and BadRequestException on error response

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,16 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Comment;
+use App\Models\Post;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 
 class CommentController extends Controller
 {
-    public function create(Request $request): JsonResponse
+    public function create(Request $request, int $postId): JsonResponse
     {
         $commentValidated = Validator::make($request->all(), [
-            'post_id' => 'required|integer',
             'comment' => 'required|string',
         ]);
 
@@ -21,8 +23,15 @@ class CommentController extends Controller
         }
 
         try {
+
+            $post = Post::find($postId);
+
+            if (!$post) {
+                throw new ModelNotFoundException('Post especificado não foi encontrado!', 404);
+            }
+
             $comment = new Comment();
-            $comment->post_id = $request->post_id;
+            $comment->post_id = $postId;
             $comment->comment = $request->comment;
             $comment->user_id = auth()->user()->id;
             $comment->save();
@@ -31,26 +40,29 @@ class CommentController extends Controller
                 'message' => 'Seu comentário foi adicionado!',
             ], 201);
         } catch (\Exception $exception) {
-            return response()->json(['error' => $exception->getMessage()], 403);
+            return response()->json(['error' => $exception->getMessage()], $exception->getCode());
         }
     }
 
     public function delete(int $commentId)
     {
         try {
-            $comment = Comment::findOrFail($commentId)->first();
-            if ($comment && auth()->user()->id == $comment->user_id) {
+            $comment = Comment::find($commentId);
+
+            if (!$comment) {
+                throw new ModelNotFoundException('Não foi possível encontrar este comentário', 404);
+            }
+
+            if (auth()->user()->id == $comment->user_id) {
                 $comment->delete();
                 return response()->json([
                     'message' => 'Seu comentário foi deletado!',
                 ], 200);
             } else {
-                return response()->json([
-                    'message' => 'Não foi possível deletar este comentário!',
-                ], 403);
+                throw new BadRequestException('Não foi possível deletar este comentário', 403);
             }
         } catch (\Exception $exception) {
-            return response()->json(['error' => 'Não foi possível encontrar este comentário'], 404);
+            return response()->json(['error' => $exception->getMessage()], $exception->getCode());
         }
 
     }
