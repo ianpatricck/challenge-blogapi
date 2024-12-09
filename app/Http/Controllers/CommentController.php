730346d84604c35d9ffcbaff283483e18ef8dc1c
ianpatricck <ianpatrick499@gmail.com>
2024-12-08 22:42:34 -0300

    feat(comment) Initial CommentController was created

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CommentController extends Controller
+{
+    public function create(Request $request): JsonResponse
+    {
+        $commentValidated = Validator::make($request->all(), [
+            'post_id' => 'required|integer',
+            'comment' => 'required|string',
+        ]);
+
+        if ($commentValidated->fails()) {
+            return response()->json($commentValidated->errors(), 403);
+        }
+
+        try {
+            $comment = new Comment();
+            $comment->post_id = $request->post_id;
+            $comment->comment = $request->comment;
+            $comment->user_id = auth()->user()->id;
+            $comment->save();
+
+            return response()->json([
+                'message' => 'Comment added successfuly',
+            ], 201);
+        } catch (\Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()], 403);
+        }
+    }
+}
