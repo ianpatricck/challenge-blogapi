ded1a4ea9e0ffa5d8364758b61db16ed5c41daa7
ianpatricck <ianpatrick499@gmail.com>
2024-12-08 22:42:16 -0300

    feat(post) Initial PostController was created

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class PostController extends Controller
+{
+    public function create(Request $request): JsonResponse
+    {
+        $postValidated = Validator::make($request->all(), [
+            'title' => 'required|string',
+            'content' => 'required|string',
+        ]);
+
+        if ($postValidated->fails()) {
+            return response()->json($postValidated->errors(), 403);
+        }
+
+        try {
+            $post = new Post();
+            $post->title = $request->title;
+            $post->content = $request->content;
+            $post->user_id = auth()->user()->id;
+            $post->save();
+
+            return response()->json([
+                'message' => 'Post foi criado com successo!',
+                'post' => $post
+            ], 201);
+        } catch (\Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()], 403);
+        }
+    }
+
+    public function update(Request $request, int $postId): JsonResponse
+    {
+        $postValidated = Validator::make($request->all(), [
+            'title' => 'required|string',
+            'content' => 'required|string',
+        ]);
+
+        if ($postValidated->fails()) {
+            return response()->json($postValidated->errors(), 403);
+        }
+
+        try {
+            $postData = Post::find($postId);
+            $updatedPost = $postData->update([
+                'title' => $request->title,
+                'content' => $request->content,
+            ]);
+
+            return response()->json([
+                'message' => 'Post atualizado com sucesso!',
+                'updated_post' => $updatedPost
+            ], 201);
+        } catch (\Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()], 403);
+        }
+    }
+
+    public function findAll(): JsonResponse
+    {
+        try {
+            $posts = Post::all();
+            return response()->json([
+                'posts' => $posts
+            ], 201);
+        } catch (\Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()], 403);
+        }
+    }
+
+    public function findOne(int $postId): JsonResponse
+    {
+        try {
+            $post = new PostResource(Post::find($postId));
+            return response()->json([
+                'post' => $post
+            ], 200);
+        } catch (\Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()], 404);
+        }
+    }
+
+    public function deleteOne(Request $request, int $postId): JsonResponse
+    {
+        try {
+            $post = Post::find($postId);
+            $post->delete();
+            return response()->json([
+                'message' => 'Post deletado com sucesso!'
+            ], 200);
+        } catch (\Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()], 404);
+        }
+    }
+}
