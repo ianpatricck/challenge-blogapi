da8b16cce8747964b2c32e1d2cc56ed90c587264
ianpatricck <ianpatrick499@gmail.com>
2024-12-09 14:49:30 -0300

    feat(Controller): start swagger API endpoint

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,16 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *     title="Blog API Documentation",
+ *     version="1.0",
+ * ),
+ *  @OA\Server(
+ *      description="Blog API",
+ *      url="http://localhost:8000/api/"
+ *  ),
+ */
 abstract class Controller
 {
     //
