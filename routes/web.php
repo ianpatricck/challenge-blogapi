afa8f9d1af15096edeeb4b968800e7328f203abb
ianpatricck <ianpatrick499@gmail.com>
2024-12-09 22:36:49 -0300

    style(web): replaced welcome view for a simple JSON response

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Bem vindo(a) ao Blog API',
+        'description' => 'Essa é uma API Rest construída a partir de um projeto Laravel que simula um blog minificado.',
+        'docs' => 'http://localhost:8000/api/documentation'
+    ]);
 });
